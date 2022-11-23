922a9d7e5d30240f00271528b565afd62af16ee5
luther90 <luther.kongne@facsciences-uy1.cm>
2022-11-23 08:34:25 +0100

    implementation du pattern de construction Factory

---
+++ factory/ProduitFactory.java
@@ -0,0 +1,24 @@
+package factory;
+
+public class ProduitFactory {
+	public static final int TYPE_PRODUITA1 = 1;
+	public static final int TYPE_PRODUITA2 = 2;
+	
+	public ProduitA getProduitA(int typeProduit) {
+		ProduitA produitA = null;
+		
+		switch(typeProduit) {
+		case TYPE_PRODUITA1:
+			produitA = new ProduitA1();
+			break;
+		case TYPE_PRODUITA2:
+			produitA = new ProduitA2();
+			break;
+		default:
+			System.out.println("n'existe pas");
+		}
+		
+		return produitA;
+		
+	}
+}
